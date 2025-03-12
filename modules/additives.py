f9f4be97397de712f67f1a3fd96ae62f5b06ddb0
pavansatya <krishvenigalla06@gmail.com>
2025-03-12 14:22:24 -0400

    updated modules and generated new plots using plotnine libraries

--- modules/additives.py
+++ modules/additives.py
@@ -8,15 +8,9 @@ import re
 #     # Split by commas and remove leading/trailing whitespace
 #     return list(set([additive.strip() for additive in str(additives).split(',')]))
 
-
-
-
 # Function to extract additive codes
 def extract_codes(text):
-    # If the text is "No additives", return it as is
     if text == 'No additives':
         return text
-    # Use regex to find all additive codes (e.g., E102, E129)
     codes = re.findall(r'E\d+\w*', text)
-    # Join the codes with commas
     return ','.join(codes)
